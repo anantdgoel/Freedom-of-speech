d61d8349027be9c65cd20f34328ef6a52612cb9e
Anant Goel <anantdgoel@gmail.com>
2017-02-11 14:56:23 -0800

    pls fix this

--- text-to-speech.py
+++ text-to-speech.py
@@ -13,4 +13,4 @@ def generateAudio(text, file_name='test', lang='en'):
     tts.save("./generated-audio/{:s}.mp3".format(file_name))
     print('Finished generating audio for text: {:s}'.format(text))
 
-generateAudio("Hello World")
+generateAudio("Hello, my name is Anant and this is my demo file")
