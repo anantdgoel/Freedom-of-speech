d3b080de3ca2dee3ed02ffb47839ac31a75221c8
Minh Hoang TCU <minh.hoang@tcu.edu>
2017-02-11 18:46:57 -0800

    finish speech to text

--- speech-to-text.py
+++ speech-to-text.py
@@ -2,17 +2,6 @@ import os
 import json
 from os.path import join, dirname
 from watson_developer_cloud import SpeechToTextV1
-import subprocess
-
-video_file = os.path.join(
-    os.path.dirname(__file__),
-    'resources',
-    'test.mp4')
-
-command = "ffmpeg -i "+video_file+" -ab 160k -ac 2 -ar 44100 -vn ./resources/audio.wav"
-
-subprocess.call(command, shell=True)
-
 
 speech_to_text = SpeechToTextV1(
     username='22ccdf7d-47bb-40fe-b175-4d2cff460f85',
@@ -20,13 +9,21 @@ speech_to_text = SpeechToTextV1(
     x_watson_learning_opt_out=False
 )
 
-print(json.dumps(speech_to_text.models(), indent=2))
+def generateTranscript(input_file_name, output_file_name='test.txt'):
+    text_file = open("./resources/" + output_file_name, "w")
+    text_file.write(getText(input_file_name))
+    text_file.close()
+    print('Finished generating transcript for audio: {:s}'
+        .format(input_file_name))
 
-print(json.dumps(speech_to_text.get_model('en-US_BroadbandModel'), indent=2))
+def getText(file_name):
+    with open(join(dirname(__file__), './resources/' + file_name),
+              'rb') as audio_file:
+        data = speech_to_text.recognize(
+            audio_file, content_type='audio/wav', timestamps=True,
+            word_confidence=True)
+        return json.dumps(data['results'][0]['alternatives'][0]['transcript'],
+            indent=2)
 
-with open(join(dirname(__file__), './resources/audio.wav'),
-          'rb') as audio_file:
-    print(json.dumps(speech_to_text.recognize(
-        audio_file, content_type='audio/wav', timestamps=True,
-        word_confidence=True),
-        indent=2))
+# print(getText('audio.wav'))
+generateTranscript('audio.wav')
